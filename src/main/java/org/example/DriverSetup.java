76aa4ade4e0d1f4581cd54702fe1c046f51fa67a
Vikram <msivikram0@gmail.com>
2026-06-15 23:31:05 +0530

    Driver Setup

---
+++ src/main/java/org/example/DriverSetup.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverSetup {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+    }
+}
